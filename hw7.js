a0e8fb818c1aea462d032a54b7ec344b4944e67a
YaraYev <yevayara@gmail.com>
2023-12-29 03:21:28 +0200

    Add home work # 7

---
+++ hw7.js
@@ -0,0 +1,68 @@
+
+let alien = {
+    egg: "A bit scary!",
+    faceHug: "Dangerous!",
+    xenoMorph: "Horror!",
+    xenoMorphWar: "Absolutely horror!",
+    queen: "EPIC FATALITY!",
+    getInfo: function () {
+        for (let i in this) {
+            if (typeof this[i] !== "function") {
+                console.log(i, ":", this[i])
+            }
+        }
+    }
+}
+
+alien.hero = "Ripley is alien slayer!"
+
+alien.getInfo()
+
+////////////////////////////////////////////////////////
+
+let services = {
+    haircut: "60 UAH",
+    shaving: "80 UAH",
+    washingHair: "100 UAH",
+    price: function () {
+        let sum = 0
+        for (let i in this) {
+            if (typeof this[i] !== "function") {
+                let b = Number.parseInt(this[i])
+                sum += b
+            }
+        }
+        return sum
+    },
+    minPrice: function () {
+        let minValue
+        for (let i in this) {
+            if (typeof this[i] !== "function") {
+                let b = Number.parseInt(this[i])
+                if (minValue === undefined || minValue > b) {
+                    minValue = b
+                }
+            }
+        }
+        return minValue
+    },
+    maxPrice: function () {
+        let maxValue
+        for (let i in this) {
+            if (typeof this[i] !== "function") {
+                let b = Number.parseInt(this[i])
+                if (maxValue === undefined || maxValue < b) {
+                    maxValue = b
+                }
+            }
+        }
+        return maxValue
+    }
+}
+
+services["breakingGlass"] = "200 UAH"
+
+console.log("Total cost of provided services:", services.price())
+console.log("Minimal price in service:", services.minPrice())
+console.log("Max price in service:", services.maxPrice())
+
